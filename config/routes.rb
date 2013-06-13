dc37f125b8d8d96ca6becb97e11e7537817f8603
liangliu <exceptionliu@gmail.com>
2013-06-13 21:40:10 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Testapp::Application.routes.draw do
+
+  get "users/new"
+
+  root :to => 'static_pages#home'
+
   get "static_pages/about"
-
   get "static_pages/home"
-
   get "static_pages/help"
+  get "static_pages/contact"
+
+  match '/about', to: 'static_pages#about'
+  match '/help', to: 'static_pages#help'
+  match '/contact', to: 'static_pages#contact'
+  match '/signup', to: 'users#new'
 
   resources :microposts
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,9 +14,9 @@ describe "StaticPages" do
       page.should have_content('Sample App')
     end
 
-    it "should have the content 'Sample App'" do
+    it "should have the title 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
     end
   end
 
@@ -29,9 +29,15 @@ describe "StaticPages" do
 
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
   end
 
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+  end
 end
